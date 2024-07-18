3e32732ff332e5322368af9e5f7da316f3c94492
Nkemdi Anyiam <NkemdiAnyiam@gmail.com>
2024-07-17 23:41:05 -0500

    Add tsconfig to testing and add set up the testing project

--- testing/scripts/index.ts
+++ testing/scripts/index.ts
@@ -13,5 +13,7 @@ const {Motion, Entrance} = WebFlik.createAnimationBanks({
   }
 });
 
-Entrance(new HTMLElement, '~pinwheel', []);
-Motion(new HTMLElement, '~move-to', [document.documentElement], {});
+const square = document.querySelector('.square');
+
+Entrance(square, '~pinwheel', []);
+Motion(square, '~move-to', [document.documentElement], {});
