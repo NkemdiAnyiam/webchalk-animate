ccbb6a8e7e223f36bfa0877260c0dcf9f52a09bc
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2024-10-07 15:01:30 -0500

    Fix bug where removal of return statements applied to all members instead of just library effect definitions

--- typedoc-config/scripts/plugins/handleLibraries.ts
+++ typedoc-config/scripts/plugins/handleLibraries.ts
@@ -5,26 +5,27 @@ const hider = (function hider() {
   // document.querySelector('a#______')?.closest('.tsd-panel.tsd-member')?.remove();
   // [...document.querySelectorAll('a[href$="#______"]')].forEach((elem) => elem.remove());
 
-  // remove interface signature and GitHub source
   if (window.location.pathname.includes(`2_animationEffects_libraryPresetBanks`)) {
+    // remove interface signature and GitHub source
     document.querySelector('.col-content > .tsd-signature')?.remove();
     document.querySelector('.col-content > .tsd-sources')?.remove();
+
+    // for each member
+    const members = [...document.querySelectorAll('.tsd-panel.tsd-member')];
+    for (const member of members) {
+      // remove member signature
+      member.querySelector(':scope > .tsd-signature')?.remove();
+      // remove signatures of objects returned by generator functions
+      member.querySelector('h5 + ul.tsd-parameters')?.remove();
+      const h5List = [...member.querySelectorAll('h5')];
+      for (const h5 of h5List) { h5.classList.add('custom-color'); }
+      // remove return title for objects returned by generator functions
+      member.querySelector('.tsd-returns-title')?.remove();
+      // remove duplicate list of config objects' properties
+      member.querySelector('li.tsd-parameter ul.tsd-parameters:has(> li.tsd-parameter > h5')?.remove();
+    }
   }
 
-  // for each member
-  const members = [...document.querySelectorAll('.tsd-panel.tsd-member')];
-  for (const member of members) {
-    // remove member signature
-    member.querySelector(':scope > .tsd-signature')?.remove();
-    // remove signatures of objects returned by generator functions
-    member.querySelector(':scope h5 + ul.tsd-parameters')?.remove();
-    const h5List = [...member.querySelectorAll(':scope h5')];
-    for (const h5 of h5List) { h5.classList.add('custom-color'); }
-    // remove return title for objects returned by generator functions
-    member.querySelector('.tsd-returns-title')?.remove();
-    // remove duplicate list of config objects' properties
-    member.querySelector(':scope li.tsd-parameter ul.tsd-parameters:has(> li.tsd-parameter > h5')?.remove();
-  }
 }).toString();
 
 export function load(app: Application) {
