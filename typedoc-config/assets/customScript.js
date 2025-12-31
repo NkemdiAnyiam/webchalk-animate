ddb14a527f21385e1efc53a16e4995445db8722d
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2025-12-30 23:22:20 -0600

    Delete 'ClipTypeToHiddenBankCategorizer' type from appearing in rendered documentation

--- typedoc-config/assets/customScript.js
+++ typedoc-config/assets/customScript.js
@@ -58,7 +58,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //   }
     // }
 
-    const bloatList = ['ValidationBloat'];
+    const bloatList = ['ValidationBloat', 'ClipTypeToHiddenBankCategorizer'];
     const tmiList = ['AnimClip'];
 
     while (true) {
