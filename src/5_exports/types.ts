f1afc8f824b14971a65ef7744e7d5c699d5b592c
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2025-12-31 16:26:35 -0600

    Remove error exports from types exports

--- src/5_exports/types.ts
+++ src/5_exports/types.ts
@@ -7,10 +7,11 @@ export type * from "../3_components/WebChalkPlaybackButtonElement";
 export type * from "../4_utils/interfaces";
 export type { EffectFrameGeneratorSet, PresetEffectDefinition, PresetEffectBank, EffectNameIn, EffectOptions } from "../2_animationEffects/customEffectCreation";
 export type { PresetLinearEasingKey, EasingString, TrivialCssEasingFunction } from "../2_animationEffects/easing";
-export type {
-  ClipErrorGenerator,
-  SequenceErrorGenerator,
-  TimelineErrorGenerator,
-  GeneralErrorGenerator,
-  CustomErrors,
-} from "../4_utils/errors";
+
+// export type {
+//   ClipErrorGenerator,
+//   SequenceErrorGenerator,
+//   TimelineErrorGenerator,
+//   GeneralErrorGenerator,
+//   CustomErrors,
+// } from "../4_utils/errors";
